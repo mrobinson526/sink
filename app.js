b0675512b65be25775359b1d81cc2fb5b2e29226
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2021-09-08 10:16:36 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,48 @@
+<script type="text/javascript">
+    var name="Margaret Robinson";
+    const states=50;
+    var sum=5+4;
+    // Data types: variable (var) and constant (const)
+    function sayHello() {
+        alert("Hello World");
+    }
+    sayHello();
+    // come back to #9
+    // Using arrays with For Loop
+    var vegetables = ["collards", "Spinach", "Broccoli", "Succotash", "Corn"];
+    for (let i=0; i<vegetables.length; i++) {
+        console.log(vegetables[i]);
+    }
+    break
+    var pet={
+        "name": "Little C",
+        "breed": "Chow",
+    }
+    Console.log(pet);
+    var people=[
+        {"name": "Mary", "age:" 34},
+        {"name:" "Susan", "age:" 25},
+        {"name:" "Jared", "age:" 18},
+        {"name:" "Jason", "age:" 17},
+        {"name:" "Jessica", "age:" 15},
+    ];
+    for (let i=0; i<people.length; i++) {
+        checkAge(people.age);
+        console.log(people[i]);
+    }
+    function getLength(str){
+        var result=getLength.str.length;
+        if (result%2) {
+            console.log("The world is nice and even!")
+        } else {
+            console.log("The world is an odd place")
+        }
+        }
+    
+    }
+    getLength("Hello World");
+
+
+
+
+</script>
